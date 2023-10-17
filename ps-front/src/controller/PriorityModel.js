4b8977ff3e85760a0d1b362e0e06739141e0e8f5
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-10-16 20:00:15 -0700

    Started working on model

--- ps-front/src/controller/PriorityModel.js
+++ ps-front/src/controller/PriorityModel.js
@@ -1,5 +1,6 @@
 export const sortPriorities = (choices) => {
-    // For now, let's just reverse the array of choices as a placeholder for actual sorting logic
-    return choices.reverse();
+    // For demonstration, let's sort the choices alphabetically
+    return choices.sort();
   };
+  
   
