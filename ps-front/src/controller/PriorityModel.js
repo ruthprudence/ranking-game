4a497635ff210ddafbdda5f169ef0d650a1308a5
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-10-16 19:53:05 -0700

    Added base files

---
+++ ps-front/src/controller/PriorityModel.js
@@ -0,0 +1,5 @@
+export const sortPriorities = (choices) => {
+    // For now, let's just reverse the array of choices as a placeholder for actual sorting logic
+    return choices.reverse();
+  };
+  
