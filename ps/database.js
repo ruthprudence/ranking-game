ef53413dc2dc5798fe817005abd5763e337bab97
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-11-23 22:52:41 -0800

    began db integration

---
+++ ps/database.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+import { createConnection } from 'mysql2';
+
+const connection = createConnection({
+    host: process.env.process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.process.env.DB_DATABASE
+});
+
+connection.connect(error => {
+    if (error) {
+        console.error('Error connectiong to the database:', error);
+        return;
+    }
+    console.log('Connected to the MySql server.');
+});
+
+export default connection;
