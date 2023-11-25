09fa1e48c50e952c19ac4beec4f961eb89623310
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-11-25 02:45:33 -0800

    added files for initial test

---
+++ ps/tests/test-server.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('./server'); // Adjust the path as needed
+
+describe('POST /submit-data', () => {
+  it('responds with a success message for valid data', async () => {
+    const response = await request(app)
+      .post('/submit-data')
+      .send({ username: 'test', ipAddress: '127.0.0.1', subjects: ['Math', 'Science'] });
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toContain('Data inserted successfully');
+  });
+});
