1e64d58df507d725524aaf8452b789440bff9e0c
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-11-25 01:21:20 -0800

    updated server.js for DB integration

--- ps/server.js
+++ ps/server.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000; //any avaiable port on my system
 
 // Define a route
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
+});
+
+// Import database connection
+const db = require('./database');
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// POST endpoint to receive data and insert into MySQL
+app.post('/submit-data', (req, res) => {
+    const data = req.body; // Data sent from the React app
+    // SQL query to insert data into the database
+    const query = 'INSERT INTO Responses SET ?';
+    db.query(query, data, (error, results, fields) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        res.send('Data inserted successfully');
+    });
 });
 
 // Start the server
