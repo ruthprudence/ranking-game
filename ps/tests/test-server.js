623342099dcdb4cdce2e56e6fd69f256a3b04841
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-11-25 22:12:47 -0800

    adding tests

--- ps/tests/test-server.js
+++ ps/tests/test-server.js
@@ -10,3 +10,10 @@ describe('POST /submit-data', () => {
     expect(response.text).toContain('Data inserted successfully');
   });
 });
+
+describe('GET /', () => {
+    if('responds with status 200', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+    });
+});
