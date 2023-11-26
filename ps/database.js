a39ad7b248928eed9fa3d0188caed9942ad71b46
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-11-26 01:56:16 -0800

    updating asychnronicity

--- ps/database.js
+++ ps/database.js
@@ -5,19 +5,34 @@
 require('dotenv').config();
 const mysql2 = require('mysql2/promise');
 
-const connection = mysql2.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+async function createDatabaseConnection() {
+    try{
+        const connection = await mysql2.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        });
 
-connection.connect(error => {
-    if (error) {
-        console.error('Error connectiong to the database:', error);
-        return;
+        console.log('Connected to the MySQL server.');
+        return connection;
+    } catch (error) {
+        console.error('Error connecting to the database', error);
+        throw error;
     }
-    console.log('Connected to the MySql server.');
-});
 
-module.exports = connection;
+
+    }
+
+
+
+
+// connection.connect(error => {
+//     if (error) {
+//         console.error('Error connectiong to the database:', error);
+//         return;
+//     }
+//     console.log('Connected to the MySql server.');
+// });
+
+module.exports = createDatabaseConnection;
