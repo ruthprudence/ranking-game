eb5441541bea83a20c4ea81ab62b1f36f2c44c8d
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-11-27 15:55:44 -0800

    serving base page locally

--- ps/server.js
+++ ps/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const {createDatabaseConnection, closeDatabaseConnection} = require('./database');
 
-app.use(express.static('public'));
+app.use(express.static('build'));
 
 const PORT = process.env.PORT || 3000;
 
@@ -46,7 +46,7 @@ app.post('/submit-data', async (req, res) => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    
+
 });
 
 module.exports = app;
