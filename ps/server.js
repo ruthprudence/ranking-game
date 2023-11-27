08637db69a5701fdc2f9fc7c70ead0f37be5085d
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-11-27 15:31:37 -0800

    working on NVM loading correct node every time

--- ps/server.js
+++ ps/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const {createDatabaseConnection, closeDatabaseConnection} = require('./database');
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 app.use(express.json());
 
 app.post('/submit-data', async (req, res) => {
