e7a6de340f422f62b4812750c3aa0f62f9f074b2
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-11-27 15:46:09 -0800

    serving static react page

--- ps/server.js
+++ ps/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const {createDatabaseConnection, closeDatabaseConnection} = require('./database');
+
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
@@ -42,6 +45,7 @@ app.post('/submit-data', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
     res.send('Hello!');
 });
 
