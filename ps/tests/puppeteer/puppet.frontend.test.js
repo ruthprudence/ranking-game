4ece4d440ef81c6228e405ecf9b06bd6778cf7cc
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-12-05 20:04:17 -0800

    installed headless browsing (puppeteer) for testing

---
+++ ps/tests/puppeteer/puppet.frontend.test.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+describe('Frontend UI Tests', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test('has a green background color', async () => {
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000'); // Replace with your app's URL
+    const backgroundColor = await page.evaluate(() => {
+      return window.getComputedStyle(document.body).backgroundColor;
+    });
+    expect(backgroundColor).toBe('rgb(0, 128, 0)'); // This is the RGB value for green
+  });
+});
