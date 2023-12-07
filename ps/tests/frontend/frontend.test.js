02b9ccf03494f14a335d0c9b7c74a2fabfcf1dd5
Ruth Prudence <ruth@Ruths-MacBook-Air.local>
2023-12-07 01:36:03 -0800

    removing unneeded tests

--- ps/tests/frontend/frontend.test.js
+++ ps/tests/frontend/frontend.test.js
@@ -1,30 +1,18 @@
 /**
  * @jest-environment jsdom
  */
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import MyComponent from '../MyComponent';
+import App from '../../src/App';
 import { server } from './mocks/server';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../../src/App';
-
 test('renders the PrioritySorter component', () => {
   render(<App />);
-//   const prioritySorterElement = screen.getByText(/rock/i);
-//   expect(prioritySorterElement).toBeInTheDocument();
-  const inputElement = screen.getByDisplayValue(/rock/i);
-expect(inputElement).toBeInTheDocument();
-
+  // Replace the following line with actual assertions relevant to your component
+  expect(screen.getByText(/some text in your component/i)).toBeInTheDocument();
 });
 
-test('', () => {});
-
-test('loads and displays data', async () => {
-  render(<MyComponent />);
-  expect(await screen.findByText('mocked value')).toBeInTheDocument();
-});
