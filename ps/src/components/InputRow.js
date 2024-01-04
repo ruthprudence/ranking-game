fe8066a22d0532f6c6daaac87b8ab195daea5858
Ruth Prudence <ruth@ruthprudence.com>
2024-01-03 22:42:21 -0800

    updated placeholder text

--- ps/src/components/InputRow.js
+++ ps/src/components/InputRow.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const InputRow = ({ index, value, onUpdate, onRemove }) => (
+const InputRow = ({ index, value, onUpdate, onRemove, placeholder }) => (
   <div>
     <span>{index + 1}. </span>
     <input
       type='text'
       value={value}
       onChange={(e) => onUpdate(index, e.target.value)}
+      placeholder={placeholder}
     />
     <button onClick={() => onRemove(index)}>Remove</button>
   </div>
