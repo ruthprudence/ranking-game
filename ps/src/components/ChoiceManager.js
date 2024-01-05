7c24c6618736c6d18230c7abdeaff81036b783f3
Ruth Prudence <ruth@ruthprudence.com>
2024-01-04 16:03:37 -0800

    refactoring to separate logic out

---
+++ ps/src/components/ChoiceManager.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// Import other necessary components or hooks
+
+const ChoiceManager = ({ pairs, rows }) => {
+  // Component logic and JSX
+  return <div> {/* JSX here */} </div>;
+};
+
+export default ChoiceManager;
