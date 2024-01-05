031c529ba8246fbd3e1fb43331c998ac93be0509
Ruth Prudence <ruth@ruthprudence.com>
2024-01-04 16:03:37 -0800

    refactoring to separate logic out

---
+++ ps/src/hooks/usePairGenerator.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+
+const usePairGenerator = (rows) => {
+  // Hook logic
+  return { /* Return pairs */ };
+};
+
+export default usePairGenerator;
---
+++ ps/src/hooks/useRowManager.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+const useRowManager = (initialRows) => {
+  // Hook logic
+  return { /* Return values and functions */ };
+};
+
+export default useRowManager;
