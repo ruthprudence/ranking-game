b5b9584952c9e255bc382c77d58eb0d1f3ab99f6
Ruth Prudence <ruth@ruthprudence.com>
2024-01-04 16:12:12 -0800

    refactoring to separate logic

--- ps/src/hooks/useRowManager.js
+++ ps/src/hooks/useRowManager.js
@@ -1,8 +1,28 @@
 import { useState } from 'react';
+import { MAXCHOICES } from '../utils/constants'; 
 
 const useRowManager = (initialRows) => {
-  // Hook logic
-  return { /* Return values and functions */ };
+  const [rows, setRows] = useState(initialRows);
+
+  const addRow = () => {
+    if (rows.length < MAXCHOICES) {
+      setRows([...rows, '']);
+    }
+  };
+
+  const removeRow = (index) => {
+    const newRows = [...rows];
+    newRows.splice(index, 1);
+    setRows(newRows);
+  };
+
+  const updateRow = (index, value) => {
+    const newRows = [...rows];
+    newRows[index] = value;
+    setRows(newRows);
+  };
+
+  return { rows, addRow, removeRow, updateRow };
 };
 
 export default useRowManager;
---
+++ ps/src/utils/constants.js
@@ -0,0 +1,1 @@
+export const MAXCHOICES = 12;
