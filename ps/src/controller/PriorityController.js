899e6d141eddd80bf5fb1f32d5bdf203e3d795e9
Ruth Prudence <ruth@ruthprudence.com>
2024-01-07 04:00:45 -0800

    updating results page

--- ps/src/controller/PriorityController.js
+++ ps/src/controller/PriorityController.js
@@ -1,7 +1,11 @@
 import { calculateScores, sortPriorities } from '../model/PriorityModel';
 
-export const handleChoiceSelection = (choices) => {
-    const scores = calculateScores(choices);
+export const handleChoiceSelection = (choices, initialScores) => {
+    if (!Array.isArray(choices)) {
+      throw new Error('choices must be an array');
+    }
+  
+    const scores = calculateScores(choices, initialScores);
     const sortedPriorities = sortPriorities(choices);
     return { scores, sortedPriorities };
-};
+  };
