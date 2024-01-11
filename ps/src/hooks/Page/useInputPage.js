3ebd011ec667260fc6af7675e65f7c7736868651
Ruth Prudence <ruth@ruthprudence.com>
2024-01-11 01:46:00 -0800

    refactoring to hooks over MVC

---
+++ ps/src/hooks/Page/useInputPage.js
@@ -0,0 +1,21 @@
+// useInputPage.js
+export const useInputPage = (setItems, goToMatchupPage) => {
+    const handleItemChange = (index, newItem) => {
+      // Logic to update items
+    };
+  
+    const handleAddItem = () => {
+      // Logic to add a new item
+    };
+  
+    const handleRemoveItem = (index) => {
+      // Logic to remove an item
+    };
+  
+    const handleSubmit = () => {
+      // Validate items and transition to matchup page
+    };
+  
+    return { handleItemChange, handleAddItem, handleRemoveItem, handleSubmit };
+  };
+  
---
+++ ps/src/hooks/Page/useResultsPage.js
@@ -0,0 +1,7 @@
+// useResultsPage.js
+export const useResultsPage = (items) => {
+    // Logic for calculating and displaying results
+  
+    return { /* Exported values and functions for results */ };
+  };
+  
