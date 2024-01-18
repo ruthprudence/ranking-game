a6c25424aaaba3c78e31b9a39c59150108bedb81
Ruth Prudence <ruth@ruthprudence.com>
2024-01-18 12:35:21 -0800

    sorting logic works, must implement testing and then work on frontend

--- ps/src/hooks/calculate/useGeneratePairs.js
+++ ps/src/hooks/calculate/useGeneratePairs.js
@@ -5,33 +5,13 @@ const generatePairs = (numItems = 0) => {
     }
 
     const pairs = [];
-
-    // Generate all possible pairs
-    for (let i = 0; i < numItems; i++) {
-        for (let j = 0; j < i; j++) {
-            pairs.push([i, j]);
+    for (let start = 1; start < numItems; start++) {
+        for (let row = start, col = 0; row < numItems; row++, col++) {
+            pairs.push([row, col]);
         }
     }
 
-    // Reorder pairs to follow the snaking pattern
-    const snakedPairs = [];
-    let start = 0;
-    let end = 0;
-
-    for (let i = 0; i < numItems - 1; i++) {
-        end += i + 1;
-        let currentPairs = pairs.slice(start, end);
-
-        if (i % 2 === 0) {
-            // Reverse the order for even rows
-            currentPairs = currentPairs.reverse();
-        }
-
-        snakedPairs.push(...currentPairs);
-        start = end;
-    }
-
-    return snakedPairs;
+    return pairs;
 };
 
 export default generatePairs;
