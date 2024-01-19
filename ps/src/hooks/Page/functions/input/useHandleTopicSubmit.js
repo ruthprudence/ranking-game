a652fa025bd56db8cad3d44dc984452466192953
Ruth Prudence <ruth@ruthprudence.com>
2024-01-18 22:40:07 -0800

    refactoring by moving functions

--- ps/src/hooks/Page/functions/input/useHandleTopicSubmit.js
+++ ps/src/hooks/Page/functions/input/useHandleTopicSubmit.js
@@ -1,10 +1,13 @@
 // utils/useHandleTopicSubmit.js
 import { useCallback } from 'react';
+import useGoToInputPage from '../nav/useGoToInputPage';
 
-const useHandleTopicSubmit = (setTopic, goToInputPage) => {
+const useHandleTopicSubmit = (setTopic, setCurrentPage) => {
+    const goToInputPage = useGoToInputPage(setTopic, setCurrentPage);
+
     return useCallback(topic => {
-        setTopic(topic);
-        goToInputPage();
+        setTopic(topic); // Set the topic first
+        goToInputPage(); // Then navigate to the input page
     }, [setTopic, goToInputPage]);
 };
 
