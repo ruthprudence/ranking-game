e5cc27a768bc4b1af98d2db153849f8b4901d8d7
Ruth Prudence <ruth@ruthprudence.com>
2024-01-20 10:31:25 -0800

    moved files for redux refactor

--- ps/src/store.js
+++ ps/src/store.js
@@ -2,7 +2,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import gameReducer from './features/game/gameSlice';
 import matchupReducer from './features/matchup/matchupSlice';
-import itemsReducer from './features/items/itemsSlice';
 import uiReducer from './features/ui/uiSlice';
 
 
@@ -10,7 +9,6 @@ export const store = configureStore({
   reducer: {
     game: gameReducer,
     matchup: matchupReducer,
-    items: itemsReducer,
     ui: uiReducer,
   },
 });
