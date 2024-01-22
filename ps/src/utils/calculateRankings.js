3e50cd91a696a607cf08b3d46f3993095e6cb55b
Ruth Prudence <ruth@ruthprudence.com>
2024-01-21 18:45:47 -0800

    moved files for redux refactor

---
+++ ps/src/utils/calculateRankings.js
@@ -0,0 +1,14 @@
+export const calculateRankings = (state) => {
+    const sortedChoices = Object.entries(state.scores).sort((a, b) => b[1] - a[1]);
+    let lastScore = null;
+    let rank = 0;
+    const rankings = sortedChoices.map(([id, score], index) => {
+        if (score !== lastScore) {
+            rank = index + 1;
+            lastScore = score;
+        }
+        const itemName = state.items.find(item => item.id.toString() === id).name;
+        return { itemName, score, rank };
+    });
+    state.rankings = rankings;
+};
