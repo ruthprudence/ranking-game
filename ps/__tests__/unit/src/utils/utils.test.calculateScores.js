ad40f4987173b1cdcf961abd5410261ea6acdc52
Ruth Prudence <ruth@ruthprudence.com>
2024-01-21 19:25:41 -0800

    moved files for redux refactor

--- ps/__tests__/unit/src/utils/utils.test.calculateScores.js
+++ ps/__tests__/unit/src/utils/utils.test.calculateScores.js
@@ -3,36 +3,18 @@ import { calculateScores } from '../../../../src/utils/calculateScores';
 describe('calculateScores utility function', () => {
     it('should correctly calculate scores from a list of items', () => {
         const state = {
-            items: [
-                { id: '1', votes: 3 },
-                { id: '2', votes: 5 },
-                { id: '3', votes: 2 }
-            ]
+            items: [{ id: '1', votes: 3 }, { id: '2', votes: 5 }, { id: '3', votes: 2 }]
         };
-        calculateScores(state);
-        expect(state.scores).toEqual({ '1': 3, '2': 5, '3': 2 });
-    });
-    
-
-    it('should handle an empty list of items', () => {
-        const state = {
-            items: [],
-            scores: {}
-        };
-        calculateScores(state);
-        expect(state.scores).toEqual({});
+        const scores = calculateScores(state);
+        expect(scores).toEqual({ '1': 3, '2': 5, '3': 2 });
     });
 
     it('should update the scores in the given state object', () => {
         const state = {
-            items: [
-                { id: '1', votes: 1 },
-                { id: '2', votes: 4 }
-            ],
-            scores: {}
+            items: [{ id: '1', votes: 1 }, { id: '2', votes: 4 }]
         };
-        calculateScores(state);
-        expect(state.scores['1']).toBe(1);
-        expect(state.scores['2']).toBe(4);
+        const scores = calculateScores(state); // Use the returned scores
+        expect(scores['1']).toBe(1); // Check the scores returned by the function
+        expect(scores['2']).toBe(4);
     });
 });
--- ps/src/utils/calculateRankings.js
+++ ps/src/utils/calculateRankings.js
@@ -1,6 +1,5 @@
 export const calculateRankings = (state) => {
     if (!state.scores) {
-        // Handle undefined scores
         return [];
     }
     const sortedChoices = Object.entries(state.scores).sort((a, b) => b[1] - a[1]);
