3d6c016232628045a77cb36648a8af47eb83ec54
Ruth Prudence <ruth@ruthprudence.com>
2024-01-22 19:38:39 -0800

    moved files for redux refactor

--- ps/src/components/Input/InputPage.js
+++ ps/src/components/Input/InputPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from '../UI/Button';
 import InputField from '../UI/InputField';
 import { useSelector, useDispatch } from 'react-redux';
-import { addRow, removeRow, updateRow} from '../../features/game/gameSlice';
+import { addRow, removeRow, updateRow} from '../../features/ui/uiSlice';
 import { submitInputPage } from '../../features/actions';
 import { initializeScores } from '../../utils/initializeScores';    
 
--- ps/src/features/ui/uiSlice.js
+++ ps/src/features/ui/uiSlice.js
@@ -5,6 +5,7 @@ import { validateRows } from '../../utils/validateRows';
 import { submitInputPage } from '../actions';
 import { initializeScores } from '../../utils/initializeScores';
 import { createAction } from '@reduxjs/toolkit';
+import { MAXCHOICES, MINCHOICES } from '../../utils/constants';
 
 export const uiSlice = createSlice({
     name: 'ui',
@@ -14,6 +15,39 @@ export const uiSlice = createSlice({
         value: '',
     },
     reducers: {
+        setTopic: (state, action) => {
+            state.topic = action.payload;
+        },
+        addItem: (state) => {
+            state.items.push('');
+        },
+        updateItem: (state, action) => {
+            const { index, value } = action.payload;
+            state.items[index] = value;
+        },
+        removeItem: (state, action) => {
+            state.items.splice(action.payload, 1);
+        },
+        setItems: (state, action) => {
+            state.items = action.payload;
+        },
+        addRow: (state) => {
+            if (state.rows.length < MAXCHOICES) {
+              state.rows.push('');
+            }
+        },
+        removeRow: (state, action) => {
+            if (state.rows.length > MINCHOICES) {
+              state.rows.splice(action.payload, 1);
+            }
+        },
+        updateRow: (state, action) => {
+            const { index, updatedValue } = action.payload;
+            state.rows[index] = updatedValue;
+        },
+        setValue: (state, action) => {
+            state.value = action.payload;
+        },
         setTopic: (state, action) => {
             state.topic = action.payload;
         },
@@ -40,6 +74,6 @@ export const uiSlice = createSlice({
 
 });
 
-export const { setTopic } = uiSlice.actions;
+export const { addItem, updateItem, removeItem, setItems, addRow, removeRow, updateRow, setValue, } = uiSlice.actions;
 export const submitTopic = createAction('ui/submitTopic');
 export default uiSlice.reducer;
