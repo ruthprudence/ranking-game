6c08ff2cdf4e4a9db10f1ffa04d9b77a6914c8b2
Ruth Prudence <ruth@ruthprudence.com>
2024-01-22 20:18:05 -0800

    moved files for redux refactor

--- ps/src/components/Splash/SplashPage.js
+++ ps/src/components/Splash/SplashPage.js
@@ -5,18 +5,22 @@ import { SplashView } from './SplashView';
 import {submitTopic } from '../../features/ui/uiSlice';
 import { setCurrentPage } from '../../features/game/gameSlice';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const SplashPage = () => {
     const [localTopic, setLocalTopic] = useState('');
     const isSubmissionSuccessful = useSelector((state) => state.ui.isSubmissionSuccessful);
     const dispatch = useDispatch();
 
-    const handleTopicSubmit = async () => {
-        await dispatch(submitTopic(localTopic));
+    const handleTopicSubmit = () => {
+        dispatch(submitTopic(localTopic));
+    };
+
+    useEffect(() => {
         if (isSubmissionSuccessful) {
             dispatch(setCurrentPage('INPUT_PAGE'));
         }
-    };
+    }, [isSubmissionSuccessful, dispatch]);
     
 
     return (
