0034234dece0a802bf32a8a80fcecd8f2c2f9dc2
Ruth Prudence <ruth@ruthprudence.com>
2024-01-25 20:19:33 -0800

    adding tests, solving for Matchup page view

--- ps/src/utils/ui/constants.js
+++ ps/src/utils/ui/constants.js
@@ -1,10 +1,14 @@
 export const MAXCHOICES = 12;
 export const MINCHOICES = 3;
 
-export const PLACEHOLDER_SPLASH = 'e.g., "Fruits"';
-export const PLACEHOLDER_PROMPT = 'Enter something to Rank';
+export const PLACEHOLDER_SPLASH = 'e.g., "fruits"';
+export const PLACEHOLDER_PROMPT = 'Rank Something!';
 
-export const PLACEHOLDER_Input = ['apricots', 'cranberries', 'persimmons'];
+export const PLACEHOLDER_Input = [
+    'apricots',
+    'persimmons',
+    'cranberries'
+];
 export const PROMPT_Input = 'Enter an item';
 
 export const ERROR_Matchup = 'Something went wrong. Please try again.';
