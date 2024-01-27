db3cf0a546ce901a453776f996a3bda1ccf5d3f5
Ruth Prudence <ruth@ruthprudence.com>
2024-01-26 18:34:08 -0800

    adding tests, solving for Splash to Input

--- ps/src/components/Splash/SplashPage.js
+++ ps/src/components/Splash/SplashPage.js
@@ -1,24 +1,24 @@
-// SplashPage.js
+// src/components/Splash/SplashPage.js
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'; 
+import { useDispatch } from 'react-redux';
 import { SplashView } from './SplashView';
+import { validateTopic } from '../../features/validate/validateSlice';
 import { submitTopic } from '../../features/ui/uiSlice';
 import { setCurrentPage } from '../../features/game/gameSlice';
-import { validateTopic } from '../../features/validate/validateSlice';
-import { PAGES } from '../../utils/ui/constants';
+import { validateTopicAsync } from '../../features/validate/validateSlice';
 
 const SplashPage = () => {
     const [localTopic, setLocalTopic] = useState('');
     const dispatch = useDispatch();
-    const topicError = useSelector((state) => state.validate.topicError);
 
     const handleTopicSubmit = async () => {
-        await dispatch(validateTopic(localTopic));
-
-        // Check if the validation passed (no error)
-        if (!topicError) {
-            dispatch(submitTopic(localTopic));
-            onAdvance();
+        try {
+            const actionResult = await dispatch(validateTopicAsync(localTopic));
+            if (validateTopicAsync.fulfilled.match(actionResult)) {
+                dispatch(setCurrentPage('INPUT_PAGE'));
+            }
+        } catch (error) {
+            console.error('Validation failed:', error);
         }
     };
 
