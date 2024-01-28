677d6980470ecdca6b0bd23984fc8713966dd5cc
Ruth Prudence <ruth@ruthprudence.com>
2024-01-27 23:25:36 -0800

    cleaning up redux slices

--- ps/src/components/Splash/SplashView.js
+++ ps/src/components/Splash/SplashView.js
@@ -4,12 +4,12 @@ import Button from '../UI/Button';
 import InputField from '../UI/InputField';
 import { PLACEHOLDER_SPLASH, PLACEHOLDER_PROMPT, PLACEHOLDERS, PROMPTS} from '../../features/constants';
 
-export const SplashView = ({ localTopic, setLocalTopic, handleTopicSubmit }) => {
+export const SplashView = ({ localTopic, setLocalTopic, handleTopicSubmit, isSubmitEnabled }) => {
     return (
         <div>
             <h1>the Ranking Game</h1>
             <InputField value={localTopic} onChange={(e) => setLocalTopic(e.target.value)} placeholder={PLACEHOLDERS.SPLASH} />
-            <Button onClick={handleTopicSubmit}>Submit Topic</Button>
+            <Button onClick={handleTopicSubmit} disabled={!isSubmitEnabled}>Submit Topic</Button>
             <p><i>{PROMPTS.SPLASH}</i></p>
         </div>
     );
