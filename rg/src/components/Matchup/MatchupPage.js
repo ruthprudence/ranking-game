f168acdd5cb9da1ea8d7254c9d5f23e00d076cc4
Ruth Prudence <ruth@ruthprudence.com>
2024-01-29 21:28:01 -0800

    redux refactor

--- rg/src/components/Matchup/MatchupPage.js
+++ rg/src/components/Matchup/MatchupPage.js
@@ -11,13 +11,19 @@ import { selectChoice, incrementVote } from '../../features/ui/uiSlice';
 const MatchupPage = () => {
   const dispatch = useDispatch();
   const isComparisonComplete = useSelector((state) => state.matchup.isComparisonComplete, shallowEqual);
-  const currentPair = useSelector(selectCurrentPair, shallowEqual);
+  const currentPair = useSelector(selectCurrentPair);
   const topic = useSelector((state) => state.ui.topic, shallowEqual);
-  const items = useSelector((state) => state.ui.items, shallowEqual);
+  const items = useSelector((state) => state.ui.items);
+
+  console.log(`
+  isComparisonComplete: ${isComparisonComplete}
+  currentPair: ${currentPair}
+  topic: ${topic}
+  items: ${items.map(item => JSON.stringify(item, null, 2))}`);
 
   useEffect(() => {
     dispatch(startMatchup(items));
-  }, [dispatch]);
+  }, [dispatch, items]);
   
   useEffect(() => {
     if (isComparisonComplete) {
@@ -26,22 +32,33 @@ const MatchupPage = () => {
   }, [isComparisonComplete, dispatch]);
 
   const handleChoiceSelect = (choiceIndex) => {
+    console.log(`handleChoiceSelect called with choiceIndex: ${choiceIndex}`);
+    
     if (currentPair && currentPair.length === 2) {
+      console.log(`currentPair: ${currentPair}`);
+      
+      const leftItem = items[currentPair[0]];
+      const rightItem = items[currentPair[1]];
+      console.log(`Item on the left: ${JSON.stringify(leftItem, null, 2)}`);
+      console.log(`Item on the right: ${JSON.stringify(rightItem, null, 2)}`);
+  
       const selectedItem = items[currentPair[choiceIndex]];
+      console.log(`User chose: ${JSON.stringify(selectedItem, null, 2)}`);
+      
       if (selectedItem) {
         dispatch(incrementVote(selectedItem)); // Passing the whole item object
+        console.log(`incrementVote dispatched with selectedItem: ${JSON.stringify(selectedItem, null, 2)}`);
+        
         dispatch(nextPair());
+        console.log('nextPair dispatched');
       }
+
+      // Output the items and their votes after the vote
+      items.forEach(item => {
+        console.log(`Item: ${item.name}, Votes: ${item.votes}`);
+      });
     }
   };
-  
-  
-  
-  
-  
-  
-
-
 
   return (
     <MatchupView 
--- rg/src/features/matchup/createItemsWithVotes.js
+++ rg/src/features/matchup/createItemsWithVotes.js
@@ -1,7 +1,7 @@
 // src/utils/createItemsWithVotes.js
 export const createItemsWithVotes = (rows) => {
     return rows.map((row, index) => ({
-        id: index,
+        id: `${index + 1} - ${row}`, 
         name: row,
         votes: 0
     }));
