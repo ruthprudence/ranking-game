2d801b3e0d5424c8a294f611196b4873e1b73f61
Ruth Prudence <ruth@ruthprudence.com>
2024-02-02 23:50:43 -0800

    updated prompts

--- rg/src/components/Matchup/MatchupView.js
+++ rg/src/components/Matchup/MatchupView.js
@@ -9,12 +9,12 @@ export const MatchupView = ({ topic, handleChoiceSelect, currentPair, items, onB
       <div className="matchup-buttons-container">
         {currentPair && currentPair.length === 2 && (
           <>
-             <Button  className="button" onClick={() => onButtonClick(0)}>{items[currentPair[0]].name}</Button>
+             <Button  className="button" onClick={() => onButtonClick(0)}>{items[currentPair[0]].name}</Button> vs 
             <Button  className="button" onClick={() => onButtonClick(1)}>{items[currentPair[1]].name}</Button>
           </>
         )}
       </div>
-      <p><i>Who Wins?</i></p>
+      <p><i>Choose the Winner!</i></p>
       <Footer /> 
     </div>
   );
