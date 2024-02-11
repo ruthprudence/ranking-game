caf845fcc0ae91bc32ebd57a515dd68d43b8ad4c
Ruth Prudence <ruth@ruthprudence.com>
2024-02-10 23:09:14 -0800

    sound updated to resolve stack

--- rg/src/features/audio/audioMiddleware.js
+++ rg/src/features/audio/audioMiddleware.js
@@ -1,44 +1,39 @@
 // src/middleware/audioMiddleware.js
 import { loadSound, soundLoaded, playSound } from './audioSlice';
-// import { SOUNDS} from '../constants';
 
 const playAudio = (soundName) => {
-  const sound = document.getElementById(soundName);
-  if (sound) {
-    sound.play().then(() => {
-      // console.log(`Sound played: ${soundName}`);
+  // Replace with the source paths to your sound files
+  const soundSrcMap = {
+    'eatGhost': '/assets/audio/pacman_eatghost.wav',
+    'eatFruit': '/assets/audio/pacman_eatfruit.wav',
+    'uhOh': '/assets/audio/pacman_death.wav',
+    'victorySound': '/assets/audio/pacman_extrapac.wav',
+    'intermission': '/assets/audio/pacman_intermission.wav'
+  };
+
+  const soundSrc = soundSrcMap[soundName];
+  if (soundSrc) {
+    const audio = new Audio(soundSrc);
+    audio.play().then(() => {
+      console.log(`Sound played: ${soundName}`);
     }).catch((error) => {
-      // console.error(`Error playing sound (${soundName}):`, error);
+      console.error(`Error playing sound (${soundName}):`, error);
     });
   } else {
-    // console.log(`Sound element not found: ${soundName}`);
+    console.log(`Sound source not found for: ${soundName}`);
   }
 };
 
 const audioMiddleware = store => next => action => {
-  // console.log(`Middleware received action: ${action.type}`, action);
-
-  if (action.type === 'audio/playSound') {
+  if (action.type === playSound.type) {
     const { name } = action.payload;
-
-    // Skip processing if the name is undefined
-    if (!name) {
-      // console.log("Skipped playing sound: Name is undefined");
-      return next(action);
-    }
-
     const state = store.getState();
-    // console.log(`Middleware processing: ${name}, Muted: ${state.audio.muted}`);
-    
     if (!state.audio.muted) {
-      // console.log(`Playing sound: ${name}`);
       playAudio(name);
     }
   }
 
   return next(action);
 };
-
-
 
 export default audioMiddleware;
