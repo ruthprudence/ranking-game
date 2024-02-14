704f5827dceb4542261fd0ef035d4053d106f5f7
Ruth Prudence <ruth@ruthprudence.com>
2024-02-14 14:34:55 -0800

    Pie updates

--- rg/src/components/UI/Pie/Pie.js
+++ rg/src/components/UI/Pie/Pie.js
@@ -1,6 +1,6 @@
 // Pie.js
 import React from 'react';
-import renderSlices from '../../../features/ui/pie/renderSlices';
+import renderSlices from '../../../features/ui/pie/renderSlices'; 
 import { useSelector, useDispatch } from 'react-redux';
 
 const Pie = () => {
@@ -9,18 +9,13 @@ const Pie = () => {
   const radius = 50;
   const viewBoxSize = 100;
 
-  // Function to handle adding a slice
-  const handleAddSlice = () => {
-    dispatch({ type: 'ADD_SLICE' });
-  };
+  // Dispatch an action when you want to add a slice (e.g., on a button click or a vote)
+  // dispatch({ type: 'ADD_SLICE' });
 
   return (
-    <>
-      <svg width={`${viewBoxSize}px`} height={`${viewBoxSize}px`} viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}>
-        {renderSlices(filledSlices, totalSlices, radius)}
-      </svg>
-      <button onClick={handleAddSlice}>Add Slice (Vote)</button>
-    </>
+    <svg width={`${viewBoxSize}px`} height={`${viewBoxSize}px`} viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}>
+      {renderSlices(filledSlices, totalSlices, radius)}
+    </svg>
   );
 };
 
