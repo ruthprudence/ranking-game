007765696497042a7d54bde1ff0e0a0f5550b7ec
Ruth Prudence <ruth@ruthprudence.com>
2024-02-26 01:19:47 -0800

    added CSS for pie

--- rg/src/components/UI/Pie/PieSlice.js
+++ rg/src/components/UI/Pie/PieSlice.js
@@ -24,7 +24,7 @@ const PieSlice = ({ index, totalSlices, radius }) => {
             d={borderPath} 
             fill="none" 
             stroke="black" 
-            strokeWidth=".5"
+            strokeWidth="1"
         />
     </>
 );
